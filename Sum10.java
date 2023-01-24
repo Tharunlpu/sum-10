6fa044190132e80cb44691d9f89c1c83f71cac4a
Tharunlpu <122251789+Tharunlpu@users.noreply.github.com>
2023-01-24 10:46:04 +0530

    Update Sum10.java

--- Sum10.java
+++ Sum10.java
@@ -3,7 +3,7 @@ class Sum10 {
         int [] arr = {1,5,9,8,2,3,4,6,7,10,11,12,13,14,15,16,17};
         for(int i=0;i<arr.length;i++) {
             for(int j=i;j<arr.length;j++) {
-                if(arr[i]+arr[j]==10) {
+                if(arr[i]+arr[j]==10||arr[i]+arr[j]==20) {
                     System.out.println(arr[i]+","+arr[j]);
                 }
             }
