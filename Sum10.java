64f74cd11a9074af01a24fac13ed4c153b51e256
Tharunlpu <122251789+Tharunlpu@users.noreply.github.com>
2023-01-24 10:43:22 +0530

    Create Sum10.java

---
+++ Sum10.java
@@ -0,0 +1,12 @@
+class Sum10 {
+    public static void main(String[] args) {
+        int [] arr = {1,5,9,8,2,3,4,6,7,10,11,12,13,14,15,16,17};
+        for(int i=0;i<arr.length;i++) {
+            for(int j=i;j<arr.length;j++) {
+                if(arr[i]+arr[j]==10) {
+                    System.out.println(arr[i]+","+arr[j]);
+                }
+            }
+        }
+    }
+}
